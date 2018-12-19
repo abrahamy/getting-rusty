507baecd0ad68dc7f06bd8bcdc5ed067046e0247
Abraham Yusuf <aaondowasey@gmail.com>
2018-12-19 15:45:54 +0100

    refact

--- src/fizz_buzz.rs
+++ src/fizz_buzz.rs
@@ -1,13 +1,11 @@
 pub fn fizz_buzz() {
   for i in 1..101 {
-    let moduli: (u8, u8) = (i % 3, i % 5);
-    let s = match moduli {
-      (0, 0) => String::from("fizzbuzz"),
-      (0, _) => String::from("fizz"),
-      (_, 0) => String::from("buzz"),
-      _ => format!("{}", i)
-    };
-    
-    println!("{}", s)
+    let t: (bool, bool) = (i % 3 == 0, i % 5 == 0);
+    match t  {
+      (true, true) => println!("fizzbuzz"),
+      (true, false) => println!("fizz"),
+      (false, true) => println!("buzz"),
+      (false, false) => println!("{}", i),
+    }
   }
 }
